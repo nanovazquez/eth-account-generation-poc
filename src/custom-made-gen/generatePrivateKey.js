7e12e346ac78340849516b0182850d108db81239
Seba Leo Perez <39063689+sebaleoperez@users.noreply.github.com>
2021-11-13 12:34:33 -0300

    Private Key Generation
    Private Key Generation using Crypto, Elliptic and Sha3

---
+++ src/custom-made-gen/generatePrivateKey.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+
+function generatePrivateKey() {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(32, (err, buf) => {
+      if (err) {       
+        reject(err);
+        return;
+      }
+
+      const privateKey = buf.toString('hex');
+      resolve(privateKey)
+    });
+  });
+}
+
+module.exports = generatePrivateKey;
+
---
+++ src/demo.js
@@ -0,0 +1,18 @@
+const generatePrivateKey = require("./custom-made-gen/generatePrivateKey");
+const getPublicKeyFromPrivateKey = require("./custom-made-gen/getPublicKeyFromPrivateKey");
+const getAddressFromPublicKey = require("./custom-made-gen/getAddressFromPublicKey");
+
+(async function main() {
+    try {
+        const privateKey = await generatePrivateKey();
+        console.log("The private key is:", privateKey);
+
+        const publicKey = getPublicKeyFromPrivateKey(privateKey);
+        console.log("The public key is:", publicKey);
+
+        const address = getAddressFromPublicKey(publicKey);
+        console.log("The address is:", address); // 0x85dAbaa40e0f455dA1a3E170A6911d88a7A90737
+    } catch(err) {
+        console.err(err);
+    }
+})();
